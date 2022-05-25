0edf515f0b61271e75aa9eb22720bdb59d564ba7
Harry <hrgus3@gmail.com>
2022-05-25 12:37:52 -0400

    Installs react-router-dom. imports the components to App.js. Creates the Switch statement in App.js.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./Components/HomePage";
+import DairyPage from "./Components/DairyPage";
+import FrozenPage from "./Components/FrozenPage";
+import GeneralMerchPage from "./Components/GeneralMerchPage";
+import GroceryPage from "./Components/GroceryPage";
 
 function App() {
-  return <div className="App">fetch('')</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/dairyPage">
+          <DairyPage />
+        </Route>
+        <Route path="/frozenPage">
+          <FrozenPage />
+        </Route>
+        <Route path="/generalMerchPage">
+          <GeneralMerchPage />
+        </Route>
+        <Route path="/groceryPage">
+          <GroceryPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
